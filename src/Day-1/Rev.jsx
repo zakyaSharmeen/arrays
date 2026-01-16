5a52a7d59e1886e7efc0076ab99045271256fb4e
zakyaSharmeen <zakyasharmeen@gmail.com>
2026-01-16 22:43:23 +0530

    first commit

--- src/Day-1/Rev.jsx
+++ src/Day-1/Rev.jsx
@@ -5,6 +5,8 @@ function Rev() {
     <div>
         <h1>hii zakya u have done it u just have to repeat this</h1>
         <p>Keep practicing and you'll get it right!</p>
+        <h1>5 arrays follows</h1>
+
     </div>
   )
 }
