27db92c10ba804fa5d0372adaaa6d519bacf42ec
zakyaSharmeen <zakyasharmeen@gmail.com>
2026-01-16 22:41:29 +0530

    first commit

---
+++ src/Day-1/Rev.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Rev() {
+  return (
+    <div>
+        <h1>hii zakya u have done it u just have to repeat this</h1>
+        <p>Keep practicing and you'll get it right!</p>
+    </div>
+  )
+}
+
+export default Rev
