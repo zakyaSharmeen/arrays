5baaf3cc63998773e8ff0db9f3516b5db02d0eb9
zakyaSharmeen <zakyasharmeen@gmail.com>
2026-01-17 22:48:22 +0530

    first commit

--- src/Day-1/Rev.jsx
+++ src/Day-1/Rev.jsx
@@ -1,14 +1,30 @@
-import React from 'react'
+import React from "react";
 
 function Rev() {
+  const numbers = [1, 2, 3, 4, 5];
+
+  //map-tranform each elemnt of array and
+  // render list and
+  // return new array
+  const simpleRenderNumbers = numbers.map((i, num) => <li key={i}>{num}</li>);
+  ////////////////////////////////////////////////////
+
+  const multipliedNumbers = numbers.map((num) => num * 2);
+  const mappedNumbered = multipliedNumbers.map((num, i) => (
+    <li key={i}>{num}</li>
+  ));
+
   return (
     <div>
-        <h1>hii zakya u have done it u just have to repeat this</h1>
-        <p>Keep practicing and you'll get it right!</p>
-        <h1>5 arrays follows</h1>
+      <ul>
+        <h1>simply rendered Numbers:</h1>
 
+        {simpleRenderNumbers}
+        <h1>multiplied numbers</h1>
+        {mappedNumbered}
+      </ul>
     </div>
-  )
+  );
 }
 
-export default Rev
+export default Rev;
---
+++ src/Rev2.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Rev2() {
+  return (
+    <div>
+        <h1>revision down </h1>
+        
+
+
+
+
+
+    </div>
+  )
+}
+
+export default Rev2
